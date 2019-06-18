e073296d35c04d4bdf2f73806007682074a0cb3d
Steisaden <steisaden@gmail.com>
2019-06-18 09:38:19 -0700

    Add files via upload

---
+++ Smooth Scroll/main.js
@@ -0,0 +1,19 @@
+// //Option 2 - JQuery Smooth Scroll
+// $('.navbar a').on('click', function(e){
+//   if(this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html', 'body').animate(
+//         {
+//         scrollTop: $(hash).offset().top
+//       }, 
+//       800
+//     );
+//   }
+// });
+//Option 3: CDN Smooth Scroll
+ const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 800
+});
